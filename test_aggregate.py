024d33c6319a0cbe16a7f7bd92fa8233512d9f0f
Michal Charemza <michal@charemza.name>
2024-01-25 08:16:47 +0000

    test: save to temporary file so it cleans up

--- test_aggregate.py
+++ test_aggregate.py
@@ -1,12 +1,12 @@
 import json
+import tempfile
 
 from aggregate import aggregate
 
 
 def test_aggregate():
-    target_file = 'aggregated.json'
-    aggregate(target_file=target_file)
+    with tempfile.NamedTemporaryFile() as f:
+        aggregate(target_file=f.name)
 
-    with open(target_file, 'rb') as f:
         results = json.loads(f.read())
-    assert 'latitude' in results
+        assert 'latitude' in results
