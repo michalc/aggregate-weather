c7695cd734379ad42d434695f3337afd68d0bfe6
Michal Charemza <michal@charemza.name>
2024-01-25 09:05:29 +0000

    test: mock source to avoid dependency on the real API in tests

--- test_aggregate.py
+++ test_aggregate.py
@@ -3,13 +3,19 @@ import tempfile
 
 import pyarrow.parquet as pq
 
-from aggregate import aggregate
+from aggregate import DEFAULT_URL, aggregate
 
 
+def test_aggregate(httpx_mock):
+    with open('fixtures/input.json', 'rb') as f:
+        httpx_mock.add_response(
+            url=DEFAULT_URL,
+            content=f.read(),
+        )
 
-def test_aggregate():
     with tempfile.NamedTemporaryFile() as f:
         aggregate(target_file=f.name)
 
         table = pq.read_table(f.name)
         assert len(table) == 38
+
