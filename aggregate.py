50b6fee1851911b04e993863f725dfddb2b879b4
Michal Charemza <michal@charemza.name>
2024-01-25 08:15:33 +0000

    style: tabs to spaces

--- aggregate.py
+++ aggregate.py
@@ -3,8 +3,8 @@ import httpx
 DEFAULT_URL='https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&hourly=temperature_2m,rain,showers,visibility&past_days=31'
 
 def aggregate(target_file, source_url=DEFAULT_URL):
-	r = httpx.get(source_url)
-	r.raise_for_status()
+    r = httpx.get(source_url)
+    r.raise_for_status()
 
-	with open(target_file, 'wb') as f:
-		f.write(r.content)
+    with open(target_file, 'wb') as f:
+        f.write(r.content)
--- test_aggregate.py
+++ test_aggregate.py
@@ -4,9 +4,9 @@ from aggregate import aggregate
 
 
 def test_aggregate():
-	target_file = 'aggregated.json'
-	aggregate(target_file=target_file)
+    target_file = 'aggregated.json'
+    aggregate(target_file=target_file)
 
-	with open(target_file, 'rb') as f:
-		results = json.loads(f.read())
-	assert 'latitude' in results
+    with open(target_file, 'rb') as f:
+        results = json.loads(f.read())
+    assert 'latitude' in results
