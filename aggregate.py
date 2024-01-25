76f8f93f831a59778fe7587d531fa5ffe16046c4
Michal Charemza <michal@charemza.name>
2024-01-25 08:06:38 +0000

    feat: rename url to source_url
    This is to make it clearer its where the data comes from. There will also be a
    "target_file" or similar argument later.

--- aggregate.py
+++ aggregate.py
@@ -2,7 +2,7 @@ import httpx
 
 DEFAULT_URL='https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&hourly=temperature_2m,rain,showers,visibility&past_days=31'
 
-def aggregate(url=DEFAULT_URL):
-	r = httpx.get(url)
+def aggregate(source_url=DEFAULT_URL):
+	r = httpx.get(source_url)
 	r.raise_for_status()
 	return r.json()
