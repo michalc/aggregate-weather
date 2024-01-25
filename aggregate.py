66b1625db0e0bd5ed9955b54fefd4beb804adc17
Michal Charemza <michal@charemza.name>
2024-01-25 08:23:25 +0000

    refactor: load json results before writing

--- aggregate.py
+++ aggregate.py
@@ -1,10 +1,15 @@
+import json
+
 import httpx
 
 DEFAULT_URL='https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&hourly=temperature_2m,rain,showers,visibility&past_days=31'
+DEFAULT_FIELDS=('rain', 'showers')
 
-def aggregate(target_file, source_url=DEFAULT_URL):
+def aggregate(target_file, source_url=DEFAULT_URL, fields=DEFAULT_FIELDS):
     r = httpx.get(source_url)
     r.raise_for_status()
 
+    source_data = r.json()
+
     with open(target_file, 'wb') as f:
-        f.write(r.content)
+        f.write(json.dumps(source_data).encode('utf-8'))
